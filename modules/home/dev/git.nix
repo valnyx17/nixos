ce8c816ca7ffc1006130b1a3ae727b475ed270fe
deva <hi@dessa.dev>
2024-08-22 18:49:28 -0400

    add: gitui

--- modules/home/dev/git.nix
+++ modules/home/dev/git.nix
@@ -1,4 +1,20 @@
 {pkgs, ...}: {
+    programs.gitui = {
+      enable = true;
+      keyConfig = ''
+(
+    move_left: Some(( code: Char('h'), modifiers: "")),
+    move_right: Some(( code: Char('l'), modifiers: "")),
+    move_up: Some(( code: Char('k'), modifiers: "")),
+    move_down: Some(( code: Char('j'), modifiers: "")),
+
+    stash_open: Some(( code: Char('l'), modifiers: "")),
+    open_help: Some(( code: F(1), modifiers: "")),
+
+    status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
+)
+    '';
+    };
     programs.git = {
       enable = true;
       package = pkgs.gitAndTools.gitFull;
