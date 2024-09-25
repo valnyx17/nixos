3cc3e4a8e33d396baf4367fe4257e40e499686e3
deva <hi@dessa.dev>
2024-09-25 19:25:21 -0400

    make home-manager use user packages

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
           }
           ./home/valerie/home.nix
         ];
