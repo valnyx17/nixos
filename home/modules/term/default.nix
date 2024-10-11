863a49a6cc1d48767a6a047c836b8ebda9d7b6a7
deva <hi@dessa.dev>
2024-10-10 20:04:52 -0400

    nord tmux

--- home/modules/term/default.nix
+++ home/modules/term/default.nix
@@ -135,27 +135,9 @@ in {
           '';
         }
         {
-          plugin = catppuccin;
+          plugin = nord;
           extraConfig = ''
             set -g status-position top
-            set -g @catppuccin_flavor 'mocha'
-
-            set -g @catppuccin_window_left_separator "█"
-            set -g @catppuccin_window_right_separator "█ "
-            set -g @catppuccin_window_number_position "right"
-            set -g @catppuccin_window_middle_separator "  █"
-
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W"
-
-            set -g @catppuccin_status_modules_right "user host session"
-            set -g @catppuccin_status_left_separator  ""
-            set -g @catppuccin_status_right_separator " "
-            set -g @catppuccin_status_fill "all"
-            set -g @catppuccin_status_connect_separator "yes"
           '';
         }
         #        {
