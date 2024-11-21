a6484738f118c3bddb932ce2faaaf282d2c70e48
valerie <hi@dessa.dev>
2024-11-20 22:49:14 -0500

    no need for kitty since i switched back to wezterm

--- home/modules/term/default.nix
+++ home/modules/term/default.nix
@@ -8,57 +8,6 @@
   runpkg = pkgs.writeShellScriptBin "run" (builtins.readFile ../../dotfiles/run);
 in {
   programs = {
-    kitty = {
-      enable = true;
-      shellIntegration.enableZshIntegration = true;
-      theme = "Nord";
-      environment = {
-        "TERM" = "xterm-256color";
-      };
-      settings = {
-        font_family = "Cartograph CF";
-        font_size = "13.0";
-        disable_ligatures = "never";
-        undercurl_style = "thick-sparse";
-        enable_audio_bell = false;
-        confirm_os_window_close = 0;
-        dynamic_background_opacity = true;
-        mouse_hide_wait = "-1.0";
-        window_padding_width = 10;
-        # background_opacity = "0.95";
-        background_blur = 5;
-        symbol_map = let
-          mappings = [
-            "U+23FB-U+23FE"
-            "U+2B58"
-            "U+E200-U+E2A9"
-            "U+E0A0-U+E0A3"
-            "U+E0B0-U+E0BF"
-            "U+E0C0-U+E0C8"
-            "U+E0CC-U+E0CF"
-            "U+E0D0-U+E0D2"
-            "U+E0D4"
-            "U+E700-U+E7C5"
-            "U+F000-U+F2E0"
-            "U+2665"
-            "U+26A1"
-            "U+F400-U+F4A8"
-            "U+F67C"
-            "U+E000-U+E00A"
-            "U+F300-U+F313"
-            "U+E5FA-U+E62B"
-          ];
-        in
-          (builtins.concatStringsSep "," mappings) + " Symbols Nerd Font";
-        input_delay = "0";
-        repaint_delay = "2";
-        sync_to_monitor = "no";
-        wayland_enable_ime = "no";
-      };
-      extraConfig = ''
-        font_features "0xProto-Italic +ss01"
-      '';
-    };
     wezterm = {
       enable = true;
       extraConfig = builtins.readFile ../../dotfiles/wezterm.lua;
